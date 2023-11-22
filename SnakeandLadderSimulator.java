bc410230c4eafb939963de8fb98bd2c8b84c5511
Vishnupriya <priya.singh9349@gmail.com>
2023-11-22 22:25:49 +0530

    Added code for Single Player at Start Position 0

--- SnakeandLadderSimulator.java
+++ SnakeandLadderSimulator.java
@@ -1,5 +1,9 @@
 public class SnakeandLadderSimulator {
     public static void main(String[] args) {
+        int position = 0;
+
         System.out.println("Welcome to Snake and Ladder Simulator");
+
+        System.out.println("Position: " + position);
     }
 }
